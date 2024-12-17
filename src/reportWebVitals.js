cdb475f1d44251af062812059755802c87456d83
Razan-creator <choukridouae944@gmail.com>
2024-12-17 09:23:55 +0100

    Voila Mon Routage

---
+++ src/reportWebVitals.js
@@ -0,0 +1,13 @@
+const reportWebVitals = onPerfEntry => {   //fonction reportWebVitals li katkhali l'ma3lomat dyal web vitals tji
+  if (onPerfEntry && onPerfEntry instanceof Function) {
+    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+      getCLS(onPerfEntry);
+      getFID(onPerfEntry);
+      getFCP(onPerfEntry);
+      getLCP(onPerfEntry);
+      getTTFB(onPerfEntry);
+    });
+  }
+};
+
+export default reportWebVitals;
